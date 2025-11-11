7db955c88e88622492abd9a79c35be0f5e55d071
pcvaz <1240908@isep.ipp.pt>
2025-11-11 21:22:41 +0000

    us 11 só falta ligar RegisterVaccineUI.cpp ao ConsoleAPP

---
+++ Core/headers/domain/model/VaccineTechnology.h
@@ -0,0 +1,24 @@
+//create by João Vaz 1240908
+#ifndef VACCINE_TECHNOLOGY_H
+#define VACCINE_TECHNOLOGY_H
+
+#include <string>
+
+/**
+ * PASSO 1: Modelo de Domínio para a Tecnologia
+ */
+class VaccineTechnology {
+private:
+    int id;
+    std::string name;
+
+public:
+    VaccineTechnology(int id, const std::string& name)
+        : id(id), name(name) {}
+
+    int getId() const { return id; }
+    std::string getName() const { return name; }
+};
+
+#endif // VACCINE_TECHNOLOGY_H
+};
